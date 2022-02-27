82903961d58e15f2e34d2abfd6f79353fa613920
jaehyeon(wiz) <eiirwp@gmail.com>
2022-02-27 17:24:34 +0900

    fix theme ui error

---
+++ pages/note/index.ts
@@ -0,0 +1,15 @@
+import type {NextPage} from "next"
+
+type NotePageProps = {
+}
+
+const NotePage: NextPage<NotePageProps> = ({
+
+}) => {
+  
+  return (
+    <div></div>
+  )
+}
+
+export default NotePage
