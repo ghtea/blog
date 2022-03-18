114c694a4aa2e87124b0f1c15db7424ddabd2d20
jaehyeon(wiz) <eiirwp@gmail.com>
2022-03-19 00:09:46 +0900

    add request in pages

--- pages/draft/index.tsx
+++ pages/draft/index.tsx
@@ -1,22 +1,39 @@
 import type {NextPage} from "next"
-import {useCallback, useState} from "react"
+import {useCallback, useMemo, useState} from "react"
+import {useMutation} from "react-query";
+import {Button} from "components/atoms/Button";
 import {Editor} from "components/organisms/Editor";
+import {createArticle} from "utils/firebase";
 
 
 type DraftPageProps = {
 }
 
 const DraftPage: NextPage<DraftPageProps> = ({
-
 }) => {
+  const {mutate: createArticleMutate, status: createArticleStatus} = useMutation(createArticle, {
+    onSuccess: () => console.log("succeeded")
+  })
+
   const [value, setValue] = useState("**Hello world!!!**")
   
   const handleChange = useCallback((value)=>{
     setValue(value)
   },[])
 
+  const handleCreate = useCallback(()=>{
+    createArticleMutate({data: {
+      title: "testing aritcle",
+      content: value,
+      tags: [],
+      isPublic: false,
+      thumbnail: ""
+    }})
+  },[createArticleMutate, value])
+
   return (
     <div>
+      <Button onClick={handleCreate}>create</Button>
       <Editor
         value={value}
         onChange={handleChange}
@@ -26,4 +43,4 @@ const DraftPage: NextPage<DraftPageProps> = ({
 }
 
 export default DraftPage
- 
+
